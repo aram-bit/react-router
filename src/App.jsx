00e33c00ff0d2b6973dc9d624fcbb43fd2e8695a
aramix <aram1988slm@gmail.com>
2025-03-06 18:55:17 +0330

    Loader & LocationList components are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
 import "./App.css"
+import LocationList from "./components/LocationList";
 function App() {
   return (
     <div>
       <Header />
+      <LocationList/>
     </div>
   );
 }
