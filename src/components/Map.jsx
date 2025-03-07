b6d848ac18bdddbce451007859a3e7bf4e9cbd1f
aramix <aram1988slm@gmail.com>
2025-03-07 10:01:08 +0330

    map center synced with lat & lng

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { useHotels } from "../contexts/HotelsProvider";
+import { useSearchParams } from "react-router-dom";
 
 function Map() {
   const [mapCenter, setMapCenter] = useState([52.3, 4.8]);
   const { isLoading, hotels } = useHotels();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  useEffect(()=>{
+   if(lat && lng) setMapCenter([lat,lng])
+  },[lat,lng])
   return (
     <div className="mapContainer">
       <MapContainer
         className="map"
-        center={mapCenter}
+        center={[lat || 52, lng || 4.8]}
         zoom={12}
         style={{ height: "700px", width: "100%" }}
       >
@@ -17,8 +24,9 @@ function Map() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://frontendi.com" data-wpel-link="internal">ARAMIX</a> contributornps'
         />
+        <ChangeCenter position={mapCenter} />
         {hotels.map((item) => (
-          <Marker key={item.id} position={[item.latitude,item.longitude]}>
+          <Marker key={item.id} position={[item.latitude, item.longitude]}>
             <Popup>{item.host_location}</Popup>
           </Marker>
         ))}
@@ -28,3 +36,8 @@ function Map() {
 }
 
 export default Map;
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
