13169ed9577b8849fb4b8142ab28d2646f2537c5
aramix <aram1988slm@gmail.com>
2025-03-07 22:09:35 +0330

    useGeoLocation hook is added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { useHotels } from "../contexts/HotelsProvider";
 import { useSearchParams } from "react-router-dom";
+import useGeoLocation from "../hooks/useGeoLocation";
 
 function Map() {
   const [mapCenter, setMapCenter] = useState([52.3, 4.8]);
@@ -9,9 +10,18 @@ function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
-  useEffect(()=>{
-   if(lat && lng) setMapCenter([lat,lng])
-  },[lat,lng])
+  const {
+    isLoading: isLoadingPosition,
+    getPosition,
+    position: geoLocationPosition,
+  } = useGeoLocation();
+  useEffect(() => {
+    if (lat && lng) setMapCenter([lat, lng]);
+  }, [lat, lng]);
+  useEffect(() => {
+    if (geoLocationPosition?.lat && geoLocationPosition?.lng)
+      setMapCenter([geoLocationPosition.lat, geoLocationPosition.lng]);
+  }, [geoLocationPosition]);
   return (
     <div className="mapContainer">
       <MapContainer
@@ -20,6 +30,9 @@ function Map() {
         zoom={12}
         style={{ height: "700px", width: "100%" }}
       >
+        <button className="getLocation" onClick={getPosition}>
+          {isLoadingPosition ? "loading..." : "use your location"}
+        </button>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://frontendi.com" data-wpel-link="internal">ARAMIX</a> contributornps'
