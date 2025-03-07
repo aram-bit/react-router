73141c92d535adbc00a72b99a7294870ac335814
aramix <aram1988slm@gmail.com>
2025-03-07 09:14:08 +0330

    hotels are shown on the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useHotels } from "../contexts/HotelsProvider";
 
 function Map() {
   const [mapCenter, setMapCenter] = useState([52.3, 4.8]);
-
+  const { isLoading, hotels } = useHotels();
   return (
     <div className="mapContainer">
       <MapContainer
@@ -16,10 +17,11 @@ function Map() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://frontendi.com" data-wpel-link="internal">ARAMIX</a> contributornps'
         />
-
-        <Marker position={mapCenter}>
-          <Popup>tehran</Popup>
-        </Marker>
+        {hotels.map((item) => (
+          <Marker key={item.id} position={[item.latitude,item.longitude]}>
+            <Popup>{item.host_location}</Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
