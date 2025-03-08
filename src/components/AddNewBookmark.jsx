3f8574853c36d6b65e17a72e77bb4a4d20ee7ff5
aramix <aram1988slm@gmail.com>
2025-03-08 19:03:57 +0330

    new location data is fetched

--- src/components/AddNewBookmark.jsx
+++ src/components/AddNewBookmark.jsx
@@ -1,21 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useUrlLocation from "../hooks/useUrlLocation";
+import axios from "axios";
+import Loader from "./Loader";
+import ReactCountryFlag from "react-country-flag";
+const BASE_GEOCODING_URL =
+  "https://api.bigdatacloud.net/data/reverse-geocode-client";
 
 function AddNewBookmark() {
   const navigate = useNavigate();
- const[lat,lng]= useUrlLocation();
+  const [lat, lng] = useUrlLocation();
+  const [cityName, setCityName] = useState("");
+  const [country, setCountry] = useState("");
+  const [countryCode, setCountryCode] = useState("");
+  const [isLoadingGeoCoding, setIsLoadingGeoCoding] = useState(false);
+  const [geoCodingError, setGeoCodingError] = useState(null);
+  if (!lat || !lng) return;
+  useEffect(() => {
+    async function fetchLocationData() {
+      setIsLoadingGeoCoding(true);
+      setIsLoadingGeoCoding(null);
+      try {
+        const { data } = await axios.get(
+          `${BASE_GEOCODING_URL}?latitude=${lat}&longitude=${lng}`
+        );
+        if (!data.countryCode) throw new Error("this location is not a city");
+        setCityName(data.city || data.locality || "");
+        setCountry(data.countryName);
+        setCountryCode(data.countryCode);
+      } catch (error) {
+        setGeoCodingError(error.message);
+      } finally {
+        setIsLoadingGeoCoding(false);
+      }
+    }
+    fetchLocationData();
+  }, [lat, lng]);
+  if (isLoadingGeoCoding) return <Loader />;
+  if (geoCodingError) return <p>this location is not a city</p>;
   return (
     <div>
       <h2>Bookmark New Locations</h2>
       <form className="form">
         <div className="formControl">
           <label htmlFor="cityName">cityName</label>
-          <input type="text" name="cityName" id="cityName" />
+          <input
+            type="text"
+            name="cityName"
+            id="cityName"
+            value={cityName}
+            onChange={(e) => setCityName(e.target.value)}
+          />
         </div>
         <div className="formControl">
           <label htmlFor="country">country</label>
-          <input type="text" name="country" id="country" />
+          <input
+            type="text"
+            name="country"
+            id="country"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+          />
+          <ReactCountryFlag svg className="flag" countryCode={countryCode} />
         </div>
         <div className="buttons">
           {" "}
